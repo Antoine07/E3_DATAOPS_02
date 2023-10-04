8053546739721524e3feeb8370df6568e5c51845
Antoine07 <antoine.lucsko@gmail.com>
2023-10-04 14:26:50 +0200

    chap1

--- Part1_Introduction/Examples/legb.py
+++ Part1_Introduction/Examples/legb.py
@@ -1,24 +1,20 @@
-
-# Variables gloables
-"""
-Définition des variables dans le script
-
-LEGB 
-"""
-
-# G espace global
 a = 101
 b = 22
 
 def foo():
     b = 1
+    print(b)
     # E englobant puis il remonte
     def baz():
         # Python b regarde localement L
+        c = 11
+        print(c)
         print(b)
         print(a)
 
     baz()
+
+    return "scope"
 
 print( foo() )
 
--- Part1_Introduction/Exercices/upper_letter.py
+++ Part1_Introduction/Exercices/upper_letter.py
@@ -3,3 +3,18 @@ Ecrire une fonction qui permet de vérifier qu'une liste de caractères ne conti
 
 """
 
+def testUpper(l):
+    # On vérifie une condition 
+    assert len(l) > 0 
+
+    for e in l:
+        if e != e.upper() :
+            return False
+
+    return True
+# testUpper([])
+try:
+    # un ternaire
+    print("Valid") if testUpper([]) else print("invalid")
+except AssertionError:
+    print("La liste est vide")
