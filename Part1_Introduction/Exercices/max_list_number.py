e6711feae77dcff3af977aecc97df858339e5f59
Antoine07 <antoine.lucsko@gmail.com>
2023-10-04 12:41:59 +0200

    exercies

--- Part1_Introduction/Exercices/max_list_number.py
+++ Part1_Introduction/Exercices/max_list_number.py
@@ -4,3 +4,22 @@ Ecrire une fonction qui permet de retourner le maximun d'une liste de nombres av
 Vous pouvez vérifiez la cohérence de votre programme en vérifiant le type et que la liste n'est pas vide.
 
 """
+
+import random
+m = []
+N = 20
+Alea = 100 
+Noise = 1_000
+for _ in range(1, N):
+    n = random.randint(1, Noise )
+    m.append( random.randint(1, n ) )
+
+print(m)
+
+elMax = m[0]
+for i in range(1, len(m)):
+    if m[i] > elMax:
+        elMax = m[i]
+
+
+print(elMax)
---
+++ Part1_Introduction/Exercices/upper_letter.py
@@ -0,0 +1,5 @@
+"""
+Ecrire une fonction qui permet de vérifier qu'une liste de caractères ne contient que des majuscules.
+
+"""
+
