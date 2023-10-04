2cf396c2530ab246a9706e59bfb2d076881a9770
Antoine07 <antoine.lucsko@gmail.com>
2023-10-04 11:11:24 +0200

    multiplication

--- Part1_Introduction/Exercices/multiplication.py
+++ Part1_Introduction/Exercices/multiplication.py
@@ -4,3 +4,19 @@ En utilisant une boucle afficher la table 7
 Puis créez une fonction pour afficher toutes les tables
 
 """ 
+
+for i in range(1, 11):
+    print( i * 7 )
+
+def mult(n):
+    l = []
+    for i in range(1, 11):
+        # print( i * n )
+        l.append( i * n )
+
+    return l 
+
+print( mult(7 ))
+print( mult(8 ))
+print( mult(2 ))
+    
