c586ef158872cff824f739a7fab3f44f993f33a7
Antoine07 <antoine.lucsko@gmail.com>
2023-10-04 11:25:16 +0200

    exercice

---
+++ Part1_Introduction/Exercices/count_str.py
@@ -0,0 +1,5 @@
+m = "mississippi"
+
+1. Comptez le nombre de "i"
+
+2. Comptez le nombre de chacune des lettres et mettre le résultat dans un dictionnaire.
--- Part1_Introduction/Exercices/ttc.py
+++ Part1_Introduction/Exercices/ttc.py
@@ -4,4 +4,7 @@ Créez une fonction qui calcule le prix TTC d'un prix HT
 """
 
 def ttc(t, p):
-    pass 
+
+    return round( (t + 1 ) * p , 2 )
+
+print( ttc( 0.2, 1314.989 ))
