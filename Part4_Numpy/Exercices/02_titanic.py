c58e72fbe997b87017516e3e72b091ea8642a381
Antoine07 <antoine.lucsko@gmail.com>
2023-10-05 14:56:31 +0200

    titanic

--- Part4_Numpy/Exercices/02_titanic.py
+++ Part4_Numpy/Exercices/02_titanic.py
@@ -1,6 +1,12 @@
 import numpy as np
 import urllib.request
 
+"""
+
+1. Donnez le nombre de femmes qui ont survécu.
+
+"""
+
 url = 'https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv'
 data = urllib.request.urlopen(url)
 
@@ -10,3 +16,5 @@ titanic_data = np.genfromtxt(data, delimiter=',', dtype=str, skip_header=1)
 
 # # Afficher les premières lignes pour vérifier le chargement
 print(titanic_data[:5])
+
+
