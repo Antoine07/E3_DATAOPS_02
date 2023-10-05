b59df306f1908380564207089ccbe649e74f6d9c
Antoine07 <antoine.lucsko@gmail.com>
2023-10-05 10:11:09 +0200

    moyenne

--- Part3_File/Exercices/NoteCoeff/main.py
+++ Part3_File/Exercices/NoteCoeff/main.py
@@ -8,24 +8,21 @@ stream = open('notes.csv', 'r',  newline='')
 
 reader = csv.reader(stream, delimiter = ',')
 
-# print( list( reader) ) 
+# print(reader)
+
 count, coeff, s = 0, 0, 0
 
 for line in reader:
-    # on saute la première ligne
+    # print(line)
     if count == 0:
         count += 1
         continue
 
-    # On récupère la note et le coeff que l'on met en float
-    n,c = float( line[0] ), float( line[1] )
-    s += n * c
-    coeff += c 
-
+    n, c = float(line[0]), float(line[1])
+    s += n*c 
+    coeff += c
     count += 1
 
+
 print(f"Moyenne : {round(s/coeff, 1)}")
-
-
-
 
